c8c37aa7991624b47634f9f9a7cb0e25c5052b4b
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-08-25 10:25:34 -0300

    Importação das rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,25 @@
 import 'dotenv/config';
 
 import express from 'express';
+import routes from './routes';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // Aplicação apta à receber requisições no formato JSON
+    this.server.use(express.json());
+  }
+
+  routes() {
+    // Importando as rotas do arquivo routes.js
+    this.server.use(routes);
   }
 }
 
