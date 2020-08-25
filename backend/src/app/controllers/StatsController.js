f5656dfac936bed77fba347fd424a5c19fe2819a
Raphael Souza Prado <raphaelpradoo@hotmail.com>
2020-08-25 15:16:16 -0300

    Status das URLs

---
+++ backend/src/app/controllers/StatsController.js
@@ -0,0 +1,23 @@
+import Site from '../models/Site';
+
+class StatsController {
+  // Index - Método para LISTAR
+  async index(req, res) {
+    // Quantidade de hits em todas as urls do sistema
+    const hits = await Site.sum('hits');
+
+    // Quantidade de urls cadastradas
+    const urlCount = await Site.count();
+
+    // 10 Urls mais acessadas
+    const topUrls = await Site.findAll({
+      limit: 5,
+      order: [['hits', 'DESC']],
+      attributes: ['id', 'hits', 'url', 'short_url'],
+    });
+
+    return res.json({ hits, urlCount, topUrls });
+  }
+}
+
+export default new StatsController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 import Router from 'express';
 
 import SiteController from './app/controllers/SiteController';
+import StatsController from './app/controllers/StatsController';
 
 const routes = new Router();
 
-// Sites
+// Rota para o exercicio 1)
 routes.post('/site', SiteController.create);
+
+// Rota para o exercicio 2)
+routes.get('/stats', StatsController.index);
 
 export default routes;
